bac5925a986cf8b58777b7298122b2fdd3284f5c
Adam Raźniewski <adam@razniewski.eu>
2020-05-12 20:19:06 +0200

    Update

--- lib/Modularus.ts
+++ lib/Modularus.ts
@@ -4,5 +4,6 @@ export interface Modularus {
     externals: any 
     baseURL: string
     loadModule(module: ModularusModule): boolean
+    loadModules(): Promise<boolean>
     addExternal(externalDependency: any): boolean
 }
