925b3266bbf7fe2299b2152f6f9df09786cb89d5
Adam Raźniewski <adam@razniewski.eu>
2020-05-12 22:08:16 +0200

    types update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,9 @@
 export { SPage } from "./SPage"
 export { SView } from "./SView"
 export { SApi } from "./SApi"
-export { UserContext } from "./UserContext"
-export { Modularus } from "./Modularus"
+import { UserContext as UC } from "./UserContext"
+export type UserContext = UC;
+import { Modularus as MRS } from "./Modularus"
+export type Modularus = MRS;
 export { ModularusComponent } from "./ModularusComponent"
 export { ModularusModule } from "./ModularusModule"
