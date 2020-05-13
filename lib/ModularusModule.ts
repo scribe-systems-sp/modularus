7be3eef61f50cba2f9dfd36dfbb27602a453daef
Adam Raźniewski <adam@razniewski.eu>
2020-05-13 16:03:50 +0200

    update

--- lib/ModularusModule.ts
+++ lib/ModularusModule.ts
@@ -9,7 +9,7 @@ export abstract class ModularusModule {
     pages?: Array<SPage>;
     views?: Array<SView>;
     translations?: any;
-    context: {};
+    context: any = {};
     daemonize: boolean = false
     
     abstract get version(): string;
