1667d8eb4e45952c5f286330eac27972b3b8b6fb
Adam Raźniewski <adam@razniewski.eu>
2020-07-14 19:18:05 +0200

    update

--- lib/SPage.ts
+++ lib/SPage.ts
@@ -4,13 +4,15 @@ export class SPage {
     translationKey: string
     priority: number
     routeKey: string // must be unique
+    layoutComponentName: string
     
-    constructor(icon: string, translationKey: string, routeKey: string, priority: number, privileges: Array<String>) {
+    constructor(icon: string, translationKey: string, routeKey: string, priority: number, privileges: Array<String>, layoutComponentName: string = "MainLayout") {
         this.icon = icon
         this.translationKey = translationKey
         this.priority = priority
         this.neededPrivileges  = privileges
         this.routeKey = routeKey
+        this.layoutComponentName = layoutComponentName
     }
 
 
