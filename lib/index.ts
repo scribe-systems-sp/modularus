eea3eca51e4f5a1d906dcd67bd30b9f8918e7ba8
Mykola Zhyhallo <mzhyhall@gmail.com>
2021-01-08 12:54:03 +0100

    Modularus main to namespace

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,34 @@
-export { SPage } from "./SPage"
-export { SView } from "./SView"
-export { SApi } from "./SApi"
+import { SPage as SP } from "./SPage"
+import { SView as SV} from "./SView"
+import { SApi as SA} from "./SApi"
 import { UserContext as UC } from "./UserContext"
-export type UserContext = UC;
 import { Modularus as MRS } from "./Modularus"
-export type Modularus = MRS;
-export { ModularusComponent } from "./ModularusComponent"
-export { ModularusModule } from "./ModularusModule"
-
-export { PrivilegeExpression } from './privilegeExpressions/privilegeExpression'
+import { ModularusComponent as MC } from "./ModularusComponent"
+import { ModularusModule as MM } from "./ModularusModule"
+import { PrivilegeExpression as PE } from './privilegeExpressions/privilegeExpression'
 import { PrivilegeValidationFunc as PVF } from './privilegeExpressions/privilegeExpression'
-export type PrivilegeValidationFunc = PVF;
-export { AND } from './privilegeExpressions/and'
-export { OR } from './privilegeExpressions/or'
-export { NOT } from './privilegeExpressions/not'
-export { FUNC } from './privilegeExpressions/func'
+import { AND as _AND } from './privilegeExpressions/and'
+import { OR as _OR } from './privilegeExpressions/or'
+import { NOT as _NOT } from './privilegeExpressions/not'
+import { FUNC as _FUNC } from './privilegeExpressions/func'
 import { ValidationFunc as VF } from './privilegeExpressions/func'
-export type ValidationFunc = VF;
+
+// Requires for sharing between modules
+namespace Modularus{
+    export type SPage = SP;
+    export type SView = SV;
+    export type SApi = SA;
+    export type UserContext = UC;
+    export type Modularus = MRS;
+    export type ModularusComponent = MC;
+    export type ModularusModule = MM;
+    export type PrivilegeExpression = PE;
+    export type PrivilegeValidationFunc = PVF;
+    export const AND = _AND;
+    export const OR = _OR;
+    export const NOT = _NOT;
+    export const FUNC = _FUNC;
+    export type ValidationFunc = VF;
+}
+
+export default Modularus
